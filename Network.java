6af9b6bece947c295667f1c91701fba994a14911
linoy hadad <linoy.hadad@post.runi.ac.il>
2025-01-08 21:08:02 +0200

    finished

--- Network.java
+++ Network.java
@@ -94,7 +94,7 @@ public class Network {
         String namemax = "";
         for (int i = 0; i < userCount; i++) {
             if (followeeCount(users[i].getName()) > max) {
-                namemax += users[i].getName();
+                namemax = namemax + users[i].getName();
 
             }
         }
@@ -115,7 +115,7 @@ public class Network {
 
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
-       String ans = "Network:\n";
+       String ans = "Network:";
        for (int i = 0; i < userCount; i++) {
         ans = ans + "\n" + users[i].toString();
        }
